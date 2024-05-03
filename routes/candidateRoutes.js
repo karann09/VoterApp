f5b16b3b1edbf6a6a44859dc6d0454cae5c4298d
karann09 <karankumar011kp@gmail.com>
2024-05-03 17:59:31 +0530

    added voting functionality

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -79,4 +79,45 @@ router.delete('/:candidateId',jwtAuthMiddleware, async (req, res)=>{
     
 });
 
+    //voting
+router.get('/vote/:candidateID', jwtAuthMiddleware, async (req, res)=>{
+    // no admin can vote
+    // user can only vote once
+    
+    candidateID = req.params.candidateID;
+    userId = req.user.id;
+
+    try{
+        // Find the Candidate document with the specified candidateID
+        const candidate = await Candidate.findById(candidateID);
+        if(!candidate){
+            return res.status(404).json({ message: 'Candidate not found' });
+        }
+
+        const user = await User.findById(userId);
+        if(!user){
+            return res.status(404).json({ message: 'user not found' });
+        }
+        if(user.role == 'admin'){
+            return res.status(403).json({ message: 'admin is not allowed'});
+        }
+        if(user.isVoted){
+            return res.status(400).json({ message: 'You have already voted' });
+        }
+
+        // Update the Candidate document to record the vote
+        candidate.votes.push({user: userId})
+        candidate.voteCount++;
+        await candidate.save();
+
+        // update the user document
+        user.isVoted = true
+        await user.save();
+
+        return res.status(200).json({ message: 'Vote recorded successfully' });
+    }catch(err){
+        console.log(err);
+        return res.status(500).json({error: 'Internal Server Error'});
+    }
+});
 module.exports = router;
